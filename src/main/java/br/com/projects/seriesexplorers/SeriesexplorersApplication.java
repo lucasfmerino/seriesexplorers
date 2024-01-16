841de4e4269a6055d9944378cfd217bac234a7f4
lucasfmerino <lucasfmerino@gmail.com>
2024-01-16 12:15:17 -0300

    feat(service): ConsumeAPI class added.

--- src/main/java/br/com/projects/seriesexplorers/SeriesexplorersApplication.java
+++ src/main/java/br/com/projects/seriesexplorers/SeriesexplorersApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.projects.seriesexplorers.service.ConsumeAPI;
+import br.com.projects.seriesexplorers.utils.Params;
+
 @SpringBootApplication
 public class SeriesexplorersApplication implements CommandLineRunner{
 
@@ -11,9 +14,22 @@ public class SeriesexplorersApplication implements CommandLineRunner{
 		SpringApplication.run(SeriesexplorersApplication.class, args);
 	}
 
+	private static String apiKey = Params.apiKey;
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("test command line runner");
+		
+		var consume = new ConsumeAPI();
+
+		// Pegar dados da série
+		var json = consume.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=" + apiKey);
+
+		System.out.println(json);
+
+
+		// Pegar imagem random
+		json = consume.getData("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 
 }
