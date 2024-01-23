b891abf685ad2a75468ff13afc0d11d790fa0727
lucasfmerino <lucasfmerino@gmail.com>
2024-01-23 08:23:04 -0300

    feat(Serie): Hibernate annotations added.

--- src/main/java/br/com/projects/seriesexplorers/model/Serie.java
+++ src/main/java/br/com/projects/seriesexplorers/model/Serie.java
@@ -3,16 +3,38 @@ package br.com.projects.seriesexplorers.model;
 import java.util.OptionalDouble;
 
 import br.com.projects.seriesexplorers.dto.SerieDTO;
+import jakarta.persistence.Column;
 // import br.com.projects.seriesexplorers.service.IntegrationGPT;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "T_SERIE")
 public class Serie {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String title;
+
     private Integer totalSeasons;
+
     private Double rating;
+
+    @Enumerated(EnumType.STRING)
     private Category genre;
+
     private String actors;
+
     private String poster;
+
     private String plot;
 
     public Serie(SerieDTO serieData) {
@@ -24,6 +46,14 @@ public class Serie {
         this.poster = serieData.seriePoster();
         // this.plot = IntegrationGPT.translate(serieData.seriePlot()).trim();
         this.plot = serieData.seriePlot();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
